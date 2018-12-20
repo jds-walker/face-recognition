0680e3294dce5e1f0849bb10737f45a5ba8dcd56
jds-walker <jds_walker@optusnet.com.au>
2018-12-20 17:38:33 +1100

    dropdown

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
 
 class ProfileIcon extends Component {
     
     constructor(props) {
         super(props);
+        this.toggle = this.toggle.bind(this);
         this.state = {
             dropdownOpen: false
         }
     }
 
+    toggle() {
+        this.setState(prevState => ({
+          dropdownOpen: !prevState.dropdownOpen
+        }));
+    }
+
     render() {
         return (
-            <div>
-                <div class="pa4 tc">
+            <div className="pa4 tc">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle
+                tag="span"
+                data-toggle="dropdown"
+                aria-expanded={this.state.dropdownOpen}
+                >
                 <img
                     src="http://tachyons.io/img/logo.jpg"
-                    class="br-100 ba h3 w3 dib" alt="avatar"/>
-                </div>
+                    className="br-100 ba h3 w3 dib" alt="avatar"/>
+            </DropdownToggle>
+            <DropdownMenu 
+                className='b--transparent shadow-5' 
+                style={{maginTop: '20px', backgroundColor: 'rgba(255,255,255,0.5)'}}>
+              <DropdownItem>View Profile</DropdownItem>
+              <DropdownItem>Sign Out</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+            
             </div>
         );
     }
